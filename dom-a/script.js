0f578313cd779b34bd0164799c096a6e76a03856
Tom Pretty <tompretty2@gmail.com>
2021-08-24 15:41:12 +0100

    specify domains when receiving/posting messages

--- dom-a/script.js
+++ dom-a/script.js
@@ -1,12 +1,18 @@
+const DOM_B_ORIGIN = "https://dom-b.thegulocal.com"
+
 window.onload = () => {
 	const iframe = document.getElementById("iframe");
 	const text = document.getElementById("ac-text")
 	
-	iframe.contentWindow.postMessage("", "*");
+	iframe.contentWindow.postMessage("", DOM_B_ORIGIN);
 
 	window.onmessage = (e) => {
-		const data = JSON.parse(e.data);
+		if (e.origin !== DOM_B_ORIGIN) {
+			return;
+		}
 
-		text.innerText = data.articleCount;
+		const { articleCount } = JSON.parse(e.data);
+
+		text.innerText = articleCount;
 	};
 }
--- dom-b/iframe.js
+++ dom-b/iframe.js
@@ -1,8 +1,14 @@
+const DOM_A_ORIGIN = "https://dom-a.thegulocal.com"
+
 window.onload = () => {
-	window.onmessage = () => {
+	window.onmessage = (e) => {
+		if (e.origin !== DOM_A_ORIGIN) {
+			return;
+		}
+
 		const parent = window.parent
 		const articleCount = parseInt(localStorage.getItem("ARTICLE_COUNT"))
 
-		parent.postMessage(JSON.stringify({ articleCount }), "*")
+		parent.postMessage(JSON.stringify({ articleCount }), DOM_A_ORIGIN)
 	}
 }
