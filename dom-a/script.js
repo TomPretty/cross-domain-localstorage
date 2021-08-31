7b5f6f83e146b6ba2e61d9ca924716c306047ef5
Tom Pretty <tompretty2@gmail.com>
2021-08-31 12:00:15 +0100

    add opting out/in

--- dom-a/script.js
+++ dom-a/script.js
@@ -1,18 +1,56 @@
-const DOM_B_ORIGIN = "https://dom-b.thegulocal.com"
+const DOM_B_ORIGIN = "https://dom-b.thegulocal.com";
 
 window.onload = () => {
-	const iframe = document.getElementById("iframe");
-	const text = document.getElementById("ac-text")
-	
-	iframe.contentWindow.postMessage("", DOM_B_ORIGIN);
+  const iframe = document.getElementById("iframe");
+  const text = document.getElementById("ac-text");
+  const optOutButton = document.getElementById("opt-out-button");
+  const optInButton = document.getElementById("opt-in-button");
 
-	window.onmessage = (e) => {
-		if (e.origin !== DOM_B_ORIGIN) {
-			return;
-		}
+  setUpClickHandlers();
+  setUpMessageHandler();
+  sendGetArticleCountMessage();
 
-		const { articleCount } = JSON.parse(e.data);
+  // ---- Helpers ---- //
 
-		text.innerText = articleCount;
-	};
-}
+  function setUpClickHandlers() {
+    optOutButton.onclick = () => sendOptOutMessage();
+    optInButton.onclick = () => sendOptInMessage();
+  }
+
+  function setUpMessageHandler() {
+    window.onmessage = (msg) => {
+      const { origin, data } = msg;
+
+      if (origin !== DOM_B_ORIGIN) {
+        return;
+      }
+
+      if (data.type === "ARTICLE_COUNT") {
+        handleArticleCount(data.articleCount);
+      }
+    };
+  }
+
+  function sendGetArticleCountMessage() {
+    iframe.contentWindow.postMessage(
+      { type: "GET_ARTICLE_COUNT" },
+      DOM_B_ORIGIN
+    );
+  }
+
+  function sendOptOutMessage() {
+    iframe.contentWindow.postMessage({ type: "OPT_OUT" }, DOM_B_ORIGIN);
+  }
+
+  function sendOptInMessage() {
+    iframe.contentWindow.postMessage({ type: "OPT_IN" }, DOM_B_ORIGIN);
+  }
+
+  function handleArticleCount(articleCount) {
+    if (articleCount.type === "OPTED_IN") {
+      text.innerText = `You have read ${articleCount.articleCount} articles`;
+    } else {
+      text.innerText = "You have opted out of article count";
+    }
+  }
+};
--- dom-b/iframe.js
+++ dom-b/iframe.js
@@ -1,14 +1,105 @@
-const DOM_A_ORIGIN = "https://dom-a.thegulocal.com"
+const DOM_A_ORIGIN = "https://dom-a.thegulocal.com";
 
 window.onload = () => {
-	window.onmessage = (e) => {
-		if (e.origin !== DOM_A_ORIGIN) {
-			return;
-		}
+  window.onmessage = (msg) => {
+    const { origin, data } = msg;
 
-		const parent = window.parent
-		const articleCount = parseInt(localStorage.getItem("ARTICLE_COUNT"))
+    if (origin !== DOM_A_ORIGIN) {
+      return;
+    }
 
-		parent.postMessage(JSON.stringify({ articleCount }), DOM_A_ORIGIN)
-	}
-}
+    if (data.type === "GET_ARTICLE_COUNT") {
+      handleGetArticleCount();
+    } else if (data.type === "OPT_OUT") {
+      handleOptOut();
+    } else if (data.type === "OPT_IN") {
+      handleOptIn();
+    }
+  };
+
+  // ---- Handlers ---- //
+
+  function handleGetArticleCount() {
+    window.parent.postMessage(
+      {
+        type: "ARTICLE_COUNT",
+        articleCount: hasOptedOutOfArticleCount()
+          ? getOptedOutMessage()
+          : getOptedInMessage(),
+      },
+      DOM_A_ORIGIN
+    );
+  }
+
+  function handleOptOut() {
+    setArticleCountOptOutCookie();
+    removeArticleCountFromLocalStorage();
+  }
+
+  function handleOptIn() {
+    deleteArticleCountOptOutCookie();
+  }
+
+  // ---- Helpers ---- //
+
+  function setArticleCountOptOutCookie() {
+    setCookie("ARTICLE_COUNT_OPT_OUT", true, 10_000);
+  }
+
+  function removeArticleCountFromLocalStorage() {
+    localStorage.removeItem("ARTICLE_COUNT");
+  }
+
+  function deleteArticleCountOptOutCookie() {
+    deleteCookie("ARTICLE_COUNT_OPT_OUT");
+  }
+
+  function hasOptedOutOfArticleCount() {
+    return checkCookie("ARTICLE_COUNT_OPT_OUT");
+  }
+
+  function getOptedOutMessage() {
+    return { type: "OPTED_OUT" };
+  }
+
+  function getOptedInMessage() {
+    const articleCount = parseInt(localStorage.getItem("ARTICLE_COUNT") ?? "0");
+
+    return {
+      type: "OPTED_IN",
+      articleCount,
+    };
+  }
+
+  // ---- Cookie utils ---- //
+
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function deleteCookie(cname) {
+    setCookie(cname, undefined, 0);
+  }
+
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  function checkCookie(cname) {
+    return !!getCookie(cname);
+  }
+};
